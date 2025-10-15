17ce608c020ca82103d5ea4eae38aefbc530829a
GuodaBal <az.guodabaltutyte@gmail.com>
2025-10-15 04:04:13 +0300

    interaction_progress

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class SceneLoader : MonoBehaviour
 {
+    public void LoadScene(string sceneName)
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
