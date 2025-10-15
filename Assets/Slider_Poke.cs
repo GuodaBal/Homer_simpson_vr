9bc2824b4b5cb00627893fb0b5d4d302c2f90c8f
GuodaBal <az.guodabaltutyte@gmail.com>
2025-10-15 04:46:14 +0300

    update

---
+++ Assets/Slider_Poke.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Slider_Poke : MonoBehaviour
+{
+    //public Transform visualTarget;
+    
+    public Vector3 movementAxis;
+    public float minDistance = 0f;
+    public float maxDistance = 0.02f;
+
+    private Transform targetTransform;
+
+    public float resetSpeed = 5;
+    public bool resetPosition = true;
+    public bool freeze = true;
+
+    private Vector3 initialPosition;
+
+    private Vector3 offset;
+    private Transform pokeAttachTransform;
+
+    private UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable interactable;
+    private bool isFollowing = false;
+    private bool isFrozen = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetTransform = GetComponent<Transform>();
+        initialPosition = targetTransform.position;
+        interactable = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactables.XRBaseInteractable>();
+        interactable.hoverEntered.AddListener(Follow);
+        interactable.hoverExited.AddListener(StopFollowing);
+        if (freeze)
+        {
+            interactable.selectEntered.AddListener(Freeze);
+        }
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFrozen) return;
+        if (isFollowing)
+        {
+            Vector3 localTargetPosition = targetTransform.InverseTransformPoint(pokeAttachTransform.position + offset);
+            Vector3 constrainedPosition = Vector3.Project(localTargetPosition, movementAxis);
+            float distance = Vector3.Dot(constrainedPosition, movementAxis.normalized);
+            distance = Mathf.Clamp(distance, minDistance, maxDistance);
+
+            Vector3 clampedLocal = movementAxis.normalized * distance;
+            targetTransform.position = targetTransform.TransformPoint(clampedLocal);
+        }
+        else if (resetPosition)
+        {
+            targetTransform.position = Vector3.Lerp(targetTransform.position, initialPosition, resetSpeed * Time.deltaTime);
+        }
+
+    }
+
+    public void Follow(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)
+        {
+            UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor interactor = (UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)hover.interactorObject;
+            isFollowing = true;
+            pokeAttachTransform = interactor.attachTransform;
+            offset = targetTransform.position - pokeAttachTransform.position;
+            isFrozen = false;
+        }
+    }
+
+    public void StopFollowing(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)
+        {
+            isFollowing = false;
+            isFrozen = false;
+        }   
+    }
+
+    public void Freeze(BaseInteractionEventArgs hover)
+    {
+        if (hover.interactorObject is UnityEngine.XR.Interaction.Toolkit.Interactors.XRPokeInteractor)
+        {
+            isFrozen = true;
+        }
+    }
+}
